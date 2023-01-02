ecb37f35223cbabe7148291e86d8ac1ff3e2b148
sreddevil123123 <sreddevil2@gmail.com>
2023-01-02 16:59:31 +0545

    added roles functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,13 +15,15 @@ const signToken = (id) => {
 
 export const signup = catchAsync(async (req, res, next) => {
   // in signup donot use create(req.body) cz auyone can assign them as admin
-  const newUser = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-  });
+  // const newUser = await User.create({
+  //   name: req.body.name,
+  //   email: req.body.email,
+  //   password: req.body.password,
+  //   confirmPassword: req.body.confirmPassword,
+  // });
+
+  const newUser = await User.create(req.body);
+
   // doing here cz after signing in we sent sucess page with token .
   const token = signToken(newUser._id);
 
@@ -63,7 +65,37 @@ export const protect = catchAsync(async (req, res, next) => {
 
   // verify token
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  console.log(decoded);
+  // check if user exists
+  const freshUser = await User.findById(decoded.id);
+  if (!freshUser)
+    return next(
+      new AppError('The user belonging to the token no longer exists', 401)
+    );
+
+  // check if user changed password
+  if (freshUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError(
+        'The user recently changed password! Please log in again',
+        401
+      )
+    );
+  }
+
+  // new user to fresh user
+  req.user = freshUser;
 
   next();
 });
+
+export const restrictTo = (...roles) => {
+  return (req, res, next) => {
+    // roles['admin','lead-guide','guide','user]
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('Yo donot have access to perform this task', 403)
+      );
+    }
+    next();
+  };
+};
