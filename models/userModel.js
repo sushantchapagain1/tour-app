9b6768e094710d75f793a20a70258dac7da49cb1
sreddevil123123 <sreddevil2@gmail.com>
2023-01-02 16:23:00 +0545

    added password changed field and changed password logic

--- models/userModel.js
+++ models/userModel.js
@@ -35,6 +35,7 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords donot match',
     },
   },
+  passwordChangedAt: Date,
 });
 
 // pre save document mongoose middleware is called after submit and before saving to db.
@@ -52,6 +53,19 @@ userSchema.methods.correctPassword = async function (
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+
+    return JWTTimestamp < changedTimestamp;
+  }
+  // False means NOT changed
+  return false;
 };
 
 const User = mongoose.model('User', userSchema);
